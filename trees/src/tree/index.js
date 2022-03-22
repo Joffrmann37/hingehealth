3e77b6d3af6fd9f9939128a3b684f2c3cd4322b4
Joffrey Mann <Joffrmann25@gmail.com>
2022-03-21 21:43:19 -0400

    Now using css to style tree.

--- trees/src/tree/index.js
+++ trees/src/tree/index.js
@@ -5,13 +5,13 @@ import axios from 'axios'
 export default function Tree() {
     return (
             <div className="tree">
-                mammals<br/>
-                &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;cheetah <br/>
-                &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;bear <br/>
-                &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;lion <br/>
-                &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;dog <br/>
-                &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;elephant <br/>
-                &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;ape <br/>
+                <span>mammals<br/></span>
+                <span className='fiveSpacer'>cheetah <br/></span>
+                <span className='fiveSpacer'>bear <br/></span>
+                <span className='tenSpacer'>lion <br/></span>
+                <span className='tenSpacer'>dog<br/></span>
+                <span className='fifteenSpacer'>elephant<br/></span>
+                <span className='fiveSpacer'>ape<br/></span>
             </div>
     )
 }
