f5e41dffb2e41b7490a6bc0ab917722ddf794824
Joffrey Mann <Joffrmann25@gmail.com>
2022-03-21 22:27:56 -0400

    Now pulling values and appropriate spacing from json file.

--- trees/src/tree/index.js
+++ trees/src/tree/index.js
@@ -1,17 +1,26 @@
 import React, {useState, useEffect} from 'react';
 import './index.css'
 import axios from 'axios'
+import data from './data.json'
 
 export default function Tree() {
+    function filteredArrByValue(val) {
+        const filteredArr = data.mammals.filter(obj => {
+            return obj.value == val
+        })
+
+        return filteredArr.length > 0 ? filteredArr[0] : ""
+    }
+    
     return (
             <div className="tree">
                 <span>mammals<br/></span>
-                <span className='fiveSpacer'>cheetah <br/></span>
-                <span className='fiveSpacer'>bear <br/></span>
-                <span className='tenSpacer'>lion <br/></span>
-                <span className='tenSpacer'>dog<br/></span>
-                <span className='fifteenSpacer'>elephant<br/></span>
-                <span className='fiveSpacer'>ape<br/></span>
+                <span style={{ marginLeft: `${filteredArrByValue("cheetah").spacing}rem`}}>{filteredArrByValue("cheetah").value}<br/></span>
+                <span style={{ marginLeft: `${filteredArrByValue("bear").spacing}rem`}}>bear <br/></span>
+                <span style={{ marginLeft: `${filteredArrByValue("lion").spacing}rem`}}>lion <br/></span>
+                <span style={{ marginLeft: `${filteredArrByValue("dog").spacing}rem`}}>dog<br/></span>
+                <span style={{ marginLeft: `${filteredArrByValue("elephant").spacing}rem`}}>elephant<br/></span>
+                <span style={{ marginLeft: `${filteredArrByValue("ape").spacing}rem`}}>ape<br/></span>
             </div>
     )
 }
