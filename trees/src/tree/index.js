10e1f7a762832e6da61cf19884cfad6e38de05bc
Joffrey Mann <Joffrmann25@gmail.com>
2022-03-22 02:49:33 -0400

    Final changes to be able to add items from the tree dynamically.

--- trees/src/tree/index.js
+++ trees/src/tree/index.js
@@ -1,26 +1,99 @@
-import React, {useState, useEffect} from 'react';
+import React, {useState, useEffect, Component} from 'react';
 import './index.css'
 import axios from 'axios'
 import data from './data.json'
+import { TextInput, StyleSheet} from 'react-native'
+import { render } from 'react-dom';
+  
+class Tree extends Component {
+    constructor() {
+        super();
+        this.state = {
+            dataArr: data.animals
+        };
+    }
 
-export default function Tree() {
-    function filteredArrByValue(val) {
-        const filteredArr = data.mammals.filter(obj => {
+    filteredArrByValue(val) {
+        const filteredArr = data.animals.filter(obj => {
             return obj.value == val
         })
 
         return filteredArr.length > 0 ? filteredArr[0] : ""
     }
+
+    filteredArrByNode(node) {
+        const filteredArr = data.animals.filter(obj => {
+            return obj.node == node
+        })
+        
+        return filteredArr.length > 0 ? filteredArr : []
+    }
+
+    doesNodeExist(node) {
+        const filteredArr = data.animals.filter(obj => {
+            return obj.node == node
+        })
+
+        return filteredArr.length > 0
+    }
+
+    insertToAnimals(obj, newVal) {
+        let data = this.state.dataArr
+        let arrAtNode = this.filteredArrByNode(obj.node)
+        const lastElementOfNode = arrAtNode[arrAtNode.length - 1]
+        const lastElement = this.state.dataArr[this.state.dataArr.length - 1]
+        const idx = obj == lastElement ? this.state.dataArr.indexOf(lastElementOfNode) : this.state.dataArr.indexOf(lastElementOfNode) + 1
+        let nextElement = this.state.dataArr[idx]
+        const nodeExists = this.doesNodeExist(nextElement.node)
+        if (obj == lastElementOfNode && nodeExists == true) {
+            this.handleInsertForNextNode(newVal, nextElement, arrAtNode)
+            return
+        }
+        let newObj = {value: newVal, spacing: lastElementOfNode.spacing, node: lastElement.node, insertNode: lastElement.insertNode}
+        data.splice(idx, 0, newObj)
+        this.setState(data);
+    }
+
+    handleInsertForNextNode(newVal, nextElement, arrAtNode) {
+        let data = this.state.dataArr
+        let newObj = {value: newVal, spacing: nextElement.spacing, node: nextElement.node, insertNode: nextElement.insertNode}
+        let arrAtNextNode = this.filteredArrByNode(nextElement.node)[this.filteredArrByNode(nextElement.node).length - 1]
+        console.log(arrAtNextNode)
+        console.log(nextElement.node)
+        const lastElementOfNextNode = arrAtNextNode[arrAtNextNode - 1]
+        const newIdx = arrAtNode.length > 1 ? this.state.dataArr.indexOf(lastElementOfNextNode) - 1 : this.state.dataArr.indexOf(lastElementOfNextNode)
+        data.splice(newIdx, 0, newObj);
+        this.setState(data)
+    }
     
-    return (
+    render() {
+        return (
             <div className="tree">
-                <span>mammals<br/></span>
-                <span style={{ marginLeft: `${filteredArrByValue("cheetah").spacing}rem`}}>{filteredArrByValue("cheetah").value}<br/></span>
-                <span style={{ marginLeft: `${filteredArrByValue("bear").spacing}rem`}}>bear <br/></span>
-                <span style={{ marginLeft: `${filteredArrByValue("lion").spacing}rem`}}>lion <br/></span>
-                <span style={{ marginLeft: `${filteredArrByValue("dog").spacing}rem`}}>dog<br/></span>
-                <span style={{ marginLeft: `${filteredArrByValue("elephant").spacing}rem`}}>elephant<br/></span>
-                <span style={{ marginLeft: `${filteredArrByValue("ape").spacing}rem`}}>ape<br/></span>
+                {
+                    this.state.dataArr.map(obj => 
+                    (<div><span style={{ marginLeft: `${obj.spacing}rem`, flex: 1, justifyContent: 'flex-start'}} dangerouslySetInnerHTML={{ __html: obj.value}} />
+                    <TextInput placeholder={`Add to ${this.filteredArrByNode(obj.node)[0].value} node`} style={styles.input}
+                        onSubmitEditing={e => {
+                            e.key === 'Enter' &&
+                            this.insertToAnimals(obj, e.nativeEvent.text)
+                            console.log(`New arr${obj.value}:`)
+                        }}></TextInput><br></br></div>)
+                    )
+                }
             </div>
-    )
+        );
+    }
 }
+
+export default Tree;
+
+const styles = StyleSheet.create({
+input: {
+    borderWidth: 1,
+    borderColor: '#FF5E34',
+    flex: 1,
+    justifyContent: 'normal',
+    borderRadius: 5,
+    textAlign: 'center'
+},
+});
